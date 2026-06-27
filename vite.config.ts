aaac208f955a5b207382f95b814bf91adf93679e
Sadulaev <sadulaev.dev@gmail.com>
2026-06-27 16:18:33 +0300

    updated build method

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin',
+  base: '/admin/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 6000,
     allowedHosts: [
